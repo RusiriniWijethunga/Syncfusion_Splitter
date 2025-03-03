39777f1747d116030cc514899c8b8f5533ef1538
RusiriniWijethunga <95708918+RusiriniWijethunga@users.noreply.github.com>
2025-03-03 22:28:51 +0530

    find the smallest value

---
+++ first.js
@@ -0,0 +1,13 @@
+function findMin(arr){
+    let min = arr[0];
+
+    for(let i=1;i<arr.length;i++){
+
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    return min;
+}
+
+console.log(findMin([6,3,4,2,3,1]))
